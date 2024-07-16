ebc36de0eda26ca9a23f47028df762ab9e912ea2
ratik123 <51818912+ratik123@users.noreply.github.com>
2024-07-16 19:07:58 +0530

    Update StringCalculator.cpp

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,9 +1,4 @@
-TEST(StringCalculatorAddTests, ExpectZeroForEmptyInput) {
-    // int expectedresult = 0;
-    // std::string input = "";
-    // StringCalculator objUnderTest;
-    // int result = objUnderTest.add(input);
-
-    // ASSERT_EQ(result, expectedresult);
-  return 0;
+int empty_String()
+{
+    return 0;
 }
