64144d7b9a981a1e173e9122ecac641c53e94c23
ratik123 <51818912+ratik123@users.noreply.github.com>
2024-07-18 11:46:25 +0530

    Update StringCalculator.cpp
    3rd code for adding 2 number , but expecting some error

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -1,9 +1,21 @@
 #include "StringCalculator.h"
+#include <sstream>
 
 int StringCalculator::add(const std::string& numbers) {
-    // If the input string is empty, return 0
-    if (numbers.empty()|| numbers == "0") {
+    // If the input string is empty or "0", return 0
+    if (numbers.empty() || numbers == "0") {
         return 0;
     }
-    return 0; 
+
+    std::stringstream ss(numbers);
+    std::string token;
+    int sum = 0;
+
+    while (std::getline(ss, token, ',')) {
+        sum += std::stoi(token);
+    }
+
+    return sum;
 }
+
+
