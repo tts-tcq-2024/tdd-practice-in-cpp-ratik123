81d13b207fc5621d5091ac887a0c3a51566822cf
ratik123 <51818912+ratik123@users.noreply.github.com>
2024-07-18 10:46:35 +0530

    Update StringCalculator.cpp
    together 1 and 2 test cases code

--- StringCalculator.cpp
+++ StringCalculator.cpp
@@ -2,15 +2,8 @@
 
 int StringCalculator::add(const std::string& numbers) {
     // If the input string is empty, return 0
-    if (numbers.empty()) {
+    if (numbers.empty()|| numbers == "0") {
         return 0;
     }
     return 0; 
-}
-
-int StringCalculator::add(const std::string& numbers) {
-    if (numbers == "0") {
-        return 0;
-    }
-    return 0; // Default return value for simplicity, can be extended for more cases
 }
